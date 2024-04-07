914599cd3cdaa45db06ad24c90d66e632e397f59
Nabil-Teamslackpull <135339987+Teamslackpull@users.noreply.github.com>
2024-04-07 18:27:21 +0200

    add via upload

---
+++ Solo-Project restaurant Ordering-App/Data.js
@@ -0,0 +1,29 @@
+
+ const MenuArray = [
+    {
+        image:"Assets/Pizza.png",
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushroom", "mozzarella"],
+        price: 14,
+        emoji: "🍕",
+        id: "PizzaID"
+
+    },
+    {
+        image: "Assets/Burger.png",
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🍔",
+        id: "HamburgerID"
+    },
+    {
+        image:"Assets/Milkshake.png",
+        name: "Milkshake",
+        ingredients: ["Milk, whipCream, Strawberry, cherry, Sprinkles"],
+        price: 9,
+        emoji: "🥤",
+        id: "MilkshakeID"
+    }
+]
+export {MenuArray}
